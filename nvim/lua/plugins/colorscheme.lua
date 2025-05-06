95b8b470bea335a21d49c16f96375fb74442ac1e
qizi <qizi0706@gmail.com>
2025-05-06 17:53:23 +0800

    add picom&nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  -- add gruvbox-material
+  -- {
+  --   'sainnhe/gruvbox-material',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.gruvbox_material_enable_italic = true
+  --     vim.cmd.colorscheme('gruvbox-material')
+  --     vim.g.gruvbox_material_background = "hard"
+  --   end
+  -- },
+
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
