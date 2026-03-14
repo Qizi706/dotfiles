6e250bd8d6af65700e9993ec21d4e59fde0f764f
Celeb <qzhou0706@gmail.com>
2026-03-14 22:16:36 +0800

    Update 2026.3.14

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,17 @@ end
 
 zoxide init fish | source
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/celeb/Programming/miniconda3/bin/conda
+    eval /home/celeb/Programming/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/celeb/Programming/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/celeb/Programming/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/celeb/Programming/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
